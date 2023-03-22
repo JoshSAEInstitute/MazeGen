5ec665114f811226de9bf3c08610a570810ff581
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2023-03-22 10:45:28 +0000

    I added side pieces walls

--- Assets/ProtoMaze.cs
+++ Assets/ProtoMaze.cs
@@ -36,24 +36,31 @@ public class ProtoMaze : MonoBehaviour
                 }
                 else if (i == 0 && j != 0) //EDGE L
                 {
-                    GameObject clone = Instantiate(mazeWall[2], new Vector3(i, 0, j), Quaternion.identity);
+                    int[] leftWall = { 2, 6, 9, 10, 12, 13, 14 };
+                    var lElement = leftWall[Random.Range(0, leftWall.Length)];
+                    GameObject clone = Instantiate(mazeWall[lElement], new Vector3(i, 0, j), Quaternion.identity);
                 }
                 else if (i != 0 && j == mazeSize - 1) //EDGE U
                 {
-                    GameObject clone = Instantiate(mazeWall[3], new Vector3(i, 0, j), Quaternion.identity);
+                    int[] upWall = {3, 6, 7, 10, 11, 13, 15 };
+                    var uElement = upWall[Random.Range(0, upWall.Length)];
+                    GameObject clone = Instantiate(mazeWall[uElement], new Vector3(i, 0, j), Quaternion.identity);
                 }
                 else if (i != 0 && j == 0) //EDGE B
                 {
-                    GameObject clone = Instantiate(mazeWall[5], new Vector3(i, 0, j), Quaternion.identity);
+                    int[] bottomWall = { 5, 8, 9, 11, 12, 13, 15 };
+                    var bElement = bottomWall[Random.Range(0, bottomWall.Length)];
+                    GameObject clone = Instantiate(mazeWall[bElement], new Vector3(i, 0, j), Quaternion.identity);
                 }
                 else if (i == mazeSize - 1 && j != 0) //EDGE R
                 {
-                    GameObject clone = Instantiate(mazeWall[4], new Vector3(i, 0, j), Quaternion.identity);
+                    int[] rightWall = { 4, 7, 8, 10, 11, 12, 14 };
+                    var rElement = rightWall[Random.Range(0, rightWall.Length)];
+                    GameObject clone = Instantiate(mazeWall[rElement], new Vector3(i, 0, j), Quaternion.identity);
                 }
                 else if ((i != 0 && j != 0) && (i != mazeSize - 1 && j != mazeSize - 1))
                 {
-                    int randomValue = Random.Range(2, 5);
-                    GameObject clone = Instantiate(mazeWall[randomValue], new Vector3(i, 0, j), Quaternion.identity);
+                    GameObject clone = Instantiate(mazeWall[Random.Range(1, 5)], new Vector3(i, 0, j), Quaternion.identity);
                 }
             }
         }
