7f234708d8855b11bb685e3ab27a23658ca17fc0
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2023-03-29 14:56:45 +0100

    It makes a path now
    + I made the path spawner trigger smaller so that they don't collide with the walls

---
+++ Assets/PathInventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathInventory : MonoBehaviour
+{
+
+    public GameObject[] upperPath;
+    public GameObject[] rightPath;
+    public GameObject[] bottomPath;
+    public GameObject[] leftPath;
+
+}
--- Assets/RoomSpawner.cs
+++ Assets/RoomSpawner.cs
@@ -1,14 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoomSpawner : MonoBehaviour
 {
 
     public int pathDirection;
-    // 1 = needs bottom path
-    // 2 = needs left path
-    // 3 = needs upper path
-    // 4 = needs right path
+    // 1 = needs BOTTOM path
+    // 2 = needs LEFT path
+    // 3 = needs UPPER path
+    // 4 = needs RIGHT path
+
+    private PathInventory inventory;
+    private int rand;
+    public bool spawned = false;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Paths").GetComponent<PathInventory>();
+
+        // Call a function using invoke after a delay
+        Invoke("Spawn", 0.1f);
+    }
+
+    private void Spawn()
+    {
+        if(spawned == false)
+        {
+            if (pathDirection == 1)
+            {
+                // Spawn room with a BOTTOM path
+
+                //Choose a random value in the bottomPath array
+                rand = Random.Range(0, inventory.bottomPath.Length);
+                //Which is then instantiated at the spawner location with no rotation
+                Instantiate(inventory.bottomPath[rand], transform.position, Quaternion.identity);
+            }
+            else if (pathDirection == 2)
+            {
+                // Spawn room with a LEFT path
+                rand = Random.Range(0, inventory.leftPath.Length);
+                Instantiate(inventory.leftPath[rand], transform.position, Quaternion.identity);
+            }
+            else if (pathDirection == 3)
+            {
+                // Spawn room with an UPPER path
+                rand = Random.Range(0, inventory.upperPath.Length);
+                Instantiate(inventory.upperPath[rand], transform.position, Quaternion.identity);
+            }
+            else if (pathDirection == 4)
+            {
+                // Spawn room with a RIGHT path
+                rand = Random.Range(0, inventory.rightPath.Length);
+                Instantiate(inventory.rightPath[rand], transform.position, Quaternion.identity);
+            }
+            spawned = true;
+        }
+ 
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Spawner") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
 
 }
