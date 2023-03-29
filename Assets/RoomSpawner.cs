6dd8e03b580d0fdab964656215fa8bbb6faadd25
JoshSAEInstitute <115077809+JoshSAEInstitute@users.noreply.github.com>
2023-03-29 13:01:22 +0100

    added spawnpoints

---
+++ Assets/RoomSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+
+    public int pathDirection;
+    // 1 = needs bottom path
+    // 2 = needs left path
+    // 3 = needs upper path
+    // 4 = needs right path
+
+}
